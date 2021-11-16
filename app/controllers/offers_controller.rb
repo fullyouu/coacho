aa1c79d4905af06f9b48eaedf0f33348e0227376
VeronikaMueller-Wilmes <vmuellerwilmes@gmail.com>
2021-11-16 12:02:27 +0100

    offer - routes-controler-view with bug

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -3,4 +3,28 @@ class OffersController < ApplicationController
   def index
     @offers = Offer.all
   end
+
+  def show
+    id = params[:id]
+    @offer = Offer.find(id)
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(strong_params)
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:offer).permit(:title, :details, :price, :duration, :category)
+  end
 end
